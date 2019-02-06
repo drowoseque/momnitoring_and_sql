0a11bdd553c785340ec554275697a60a4865c133
Mikhail Slavoshevskiy <MESlavoshevskiy.SBT@sberbank.ru>
2019-02-06 21:01:49 +0300

    connection pool

--- mas/helpers/postgres.py
+++ mas/helpers/postgres.py
@@ -3,25 +3,28 @@ from typing import Tuple, Any, List, Optional
 import asyncpg
 from asyncpg import Record
 
-from asyncpg.connection import Connection
-
+from asyncpg.pool import Pool
 from mas.settings import POSTGRES_CONNECTION_CONF
 
 
-async def connect() -> Connection:
-    return await asyncpg.connect(**POSTGRES_CONNECTION_CONF)
+async def create_pool() -> Pool:
+    return await asyncpg.create_pool(**POSTGRES_CONNECTION_CONF)
 
 
-_connection = None
+_pool = None
+
+
+async def get_pool() -> Pool:
+    global _pool
+    if _pool is None:
+        _pool = await create_pool()
+    return _pool
 
 
 async def execute(
         *,
         query: str,
-        params: Optional[Tuple[Any, ...]] = None,
+        params: Optional[Tuple[Any, ...]] = None
 ) -> List[Record]:
-    global _connection
-    if _connection is None:
-        _connection = await connect()
-
-    return await _connection.fetch(query, *params)
+    async with _pool.acquire() as connection:
+        return await connection.fetch(query, *params)
