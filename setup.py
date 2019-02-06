02f95589eabaaf54525405427ed3cfdd854d92b6
Mikhail Slavoshevskiy <MESlavoshevskiy.SBT@sberbank.ru>
2019-02-06 16:45:07 +0300

    initial

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='monitoring_and_sql',
+    version='1.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='slavoshevskiy-me',
+    author_email='slavoshevskii.mihail@phystech.edu',
+    description='',
+    install_requires=[
+        'click==6.7',
+        'tornado==5.0.2',
+    ]
+)
